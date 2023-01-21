fdb771c60a73c7fdea07121f17788e29ee1ce290
saifuddin <saifyea@gmail.com>
2023-01-21 16:12:29 +0600

    Database & Migration completed

---
+++ database/migrations/2023_01_21_100727_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Brick\Math\BigInteger;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('emp_id');
+            $table->string('emp_name');
+            $table->string('emp_designation');
+            $table->string('emp_joindate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
